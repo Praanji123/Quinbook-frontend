59cf0aab377d95f136e8debe15922a4514bb2f79
Prateek Srivastava <prateeksrivastava@Prateeks-MacBook-Pro.local>
2021-03-09 13:00:25 +0530

    registerpage

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
-
+import register from '@/views/register.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path: '/register',
+    name: 'Register',
+    component: register
+  }
 ]
 
 const router = new VueRouter({
